25abedbda2e163de4467a3643f9893f9ca59022a
ekkkk1 <asdqwek200@naver.com>
2021-03-05 00:56:33 +0900

    profile update test

---
+++ src/test/java/com/studyolle/settings/SettingsControllerTest.java
@@ -0,0 +1,116 @@
+package com.studyolle.settings;
+
+import com.studyolle.WithAccount;
+import com.studyolle.account.AccountRepository;
+import com.studyolle.account.AccountService;
+import com.studyolle.account.SignUpForm;
+import com.studyolle.domain.Account;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SettingsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname("gicheolWithUserDetails");
+        signUpForm.setEmail("gicheol@a.com");
+        signUpForm.setPassword("12345678");
+        accountService.processNewAccount(signUpForm);
+    }
+
+    @AfterEach
+    void afterEach() {
+        accountRepository.deleteAll();
+    }
+
+    @WithAccount("gicheol")
+    @DisplayName("프로필 수정 폼")
+    @Test
+    void updateProfileForm() throws Exception {
+        String bio = "짧은 소개를 수정하는 경우.";
+        mockMvc.perform(get(SettingsController.SETTINGS_PROFILE_URL))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"))
+        ;
+    }
+
+    @WithAccount("gicheolWithAccount")
+    @DisplayName("프로필 수정하기 - 입력값 정상 (방법 1 : @WithAccount 정의해서 사용. 이 경우 AfterEach 필수)")
+    @Test
+    void updateProfile1() throws Exception {
+        String bio = "짧은 소개를 수정하는 경우.";
+        mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
+                .param("bio", bio)
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(SettingsController.SETTINGS_PROFILE_URL))
+                .andExpect(flash().attributeExists("message"))
+        ;
+
+        Account gicheol = accountRepository.findByNickname("gicheolWithAccount");
+        assertEquals(bio, gicheol.getBio());
+    }
+
+    @WithUserDetails(value = "gicheolWithUserDetails", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("프로필 수정하기 - 입력값 정상 (방법 2 : @WithUserDetails 사용)")
+    @Test
+    void updateProfile2() throws Exception {
+        String bio = "짧은 소개를 수정하는 경우.";
+        mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
+                .param("bio", bio)
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(SettingsController.SETTINGS_PROFILE_URL))
+                .andExpect(flash().attributeExists("message"))
+        ;
+
+        Account gicheol = accountRepository.findByNickname("gicheolWithUserDetails");
+        assertEquals(bio, gicheol.getBio());
+    }
+
+
+
+    @WithAccount("gicheolError")
+    @DisplayName("프로필 수정하기 - 입력값 에러")
+    @Test
+    void updateProfile_error() throws Exception {
+        String bio = "길게 소개를 수정하는 경우. 길게 소개를 수정하는 경우. 길게 소개를 수정하는 경우. 길게 소개를 수정하는 경우. 길게 소개를 수정하는 경우. 길게 소개를 수정하는 경우. 길게 소개를 수정하는 경우.";
+        mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
+                .param("bio", bio)
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PROFILE_VIEW_NAME))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"))
+                .andExpect(model().hasErrors())
+        ;
+
+        Account gicheol = accountRepository.findByNickname("gicheolError");
+        assertNull(gicheol.getBio());
+    }
+
+}
