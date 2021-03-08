57aee5b7696e418bb943aaf0c7149836a4a44730
ekkkk1 <asdqwek200@naver.com>
2021-03-09 00:39:19 +0900

    update password

--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -98,9 +98,12 @@ public class AccountService implements UserDetailsService {
         account.setOccupation(profile.getOccupation());
         account.setLocation(profile.getLocation());
         account.setBio(profile.getBio());
-        // TODO Profile Image
-
+        account.setProfileImage(profile.getProfileImage());
         accountRepository.save(account);
     }
 
+    public void updatePassword(Account account, String newPassword) {
+        account.setPassword(passwordEncoder.encode(newPassword));
+        accountRepository.save(account);
+    }
 }
--- src/main/java/com/studyolle/settings/PasswordForm.java
+++ src/main/java/com/studyolle/settings/PasswordForm.java
@@ -1,2 +1,15 @@
-package com.studyolle.settings;public class PasswordForm {
+package com.studyolle.settings;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class PasswordForm {
+
+    @Length(min = 8, max = 50)
+    private String newPassword;
+
+    @Length(min = 8, max = 50)
+    private String newPasswordConfirm;
+
 }
